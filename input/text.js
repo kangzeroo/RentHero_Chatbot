07d64e8089e4db3c6cd4a8e828766726bfde6407
jmalliaros <jake.malliaros@gmail.com>
2018-06-21 17:11:04 -0400

    dfTrainer and main folder upload

---
+++ input/text.js
@@ -0,0 +1,7 @@
+module.exports = `
+  Hi, I’m wondering if a room is still available?
+  Is furniture included?
+  Are you there?
+  I have a group of 3 guys from WLU, looking to rent for 8 months if possible.
+  Also are pets allowed? Thanks!
+`
